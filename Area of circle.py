54db59da5bf7f7536d8508f67510904c5fb4cec7
Nabeeha-26 <100037246+Nabeeha-26@users.noreply.github.com>
2023-02-11 10:48:49 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 22:37:49 2023
+
+@author: Nabeeha
+"""
+
---
+++ extension .py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))

f75af4b0395a924019f772f5dcfa44258c7c297f
Nabeeha-26 <100037246+Nabeeha-26@users.noreply.github.com>
2023-02-11 11:11:01 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 10:57:27 2023
+
+@author: Nabeeha
+"""
+
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
